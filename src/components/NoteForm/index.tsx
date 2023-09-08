1e5c2f2f3521da153fdd673c8b4bb9b3cadcb740
Craig McNaughton <therealcraigmcn@gmail.com>
2023-09-08 10:55:25 -0700

    Update NoteForm with data and event handlers
    - update Note title input to text
    - disable Form button for blank input
      - disallow Submit for blank input (suspenders and a belt)

--- src/components/NoteForm/index.tsx
+++ src/components/NoteForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./index.css";
 import { Note } from '../../types/Note';
 
@@ -11,10 +11,31 @@ const NoteForm: React.FC<NoteFormProps> = ({ onSubmit, noteToEdit }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
-  useEffect(() => {});
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!title || !content) {
+      return;
+    }
+
+    onSubmit({ id: noteToEdit?.id || 0, title, content });
+
+    setTitle('');
+    setContent('');
   };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  }
+
+  useEffect(() => {
+    setTitle(noteToEdit?.title || '');
+    setContent(noteToEdit?.content || '');
+  }, [noteToEdit]);
 
   return (
     <div className="layout-column align-items-center justify-content-start" data-testid="note-manager">
@@ -23,12 +44,12 @@ const NoteForm: React.FC<NoteFormProps> = ({ onSubmit, noteToEdit }) => {
           <section className="layout-row align-items-center justify-content-center mt-20 mr-20 ml-20">
             <label className="form-title-label">Title:</label>
             <input
-              type="number"
+              type="text"
               placeholder="Title"
               value={title}
               data-testid="form-input"
               className="form-input"
-              onChange={() => { }}
+              onChange={handleTitleChange}
             />
           </section>
           <section className="layout-row align-items-center justify-content-center mt-20 mr-20 ml-20">
@@ -38,11 +59,11 @@ const NoteForm: React.FC<NoteFormProps> = ({ onSubmit, noteToEdit }) => {
               value={content}
               data-testid="form-textarea"
               className="form-textarea"
-              onChange={() => { }}
+              onChange={handleContentChange}
             />
           </section>
           <section className="layout-row align-items-center justify-content-center mt-20 mr-20 ml-20">
-            <button data-testid="form-submit-button" type="submit" disabled={false}>{noteToEdit ? 'Update' : 'Add'}</button>
+            <button data-testid="form-submit-button" type="submit" disabled={!title || !content}>{noteToEdit ? 'Update' : 'Add'}</button>
           </section>
         </form>
       </div>
