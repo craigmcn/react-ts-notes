0caf04a34fb3c16e629783892609d938771ea18a
Craig McNaughton <therealcraigmcn@gmail.com>
2023-09-08 10:55:38 -0700

    Update NoteManager with data and event handlers
    - create nextNoteId function to ensure unique-ish, sequential IDs
    - allow `undefined` `noteToEdit`

--- src/components/NoteManager/index.tsx
+++ src/components/NoteManager/index.tsx
@@ -3,14 +3,45 @@ import { Note } from '../../types/Note';
 import NoteForm from '../NoteForm';
 import NoteTable from '../NoteTable';
 
+const nextNoteId = (notes: Note[]) => {
+  const maxId = notes.reduce((max, note) => Math.max(max, note.id || 0), 0);
+  return maxId + 1;
+}
+
 const NoteManager: React.FC = () => {
   const [notes, setNotes] = useState<Note[]>([]);
-  const [noteToEdit, setNoteToEdit] = useState<Note | null>(null);
+  const [noteToEdit, setNoteToEdit] = useState<Note>();
+
+  const handleSubmit = (note: Note) => {
+    setNoteToEdit(undefined);
+
+    if (note.id === 0) {
+      note.id = nextNoteId(notes);
+      setNotes([...notes, note]);
+      return
+    }
+    
+    const updatedNotes = notes.map((n) => {
+      if (n.id === note.id) {
+        return note;
+      }
+      return n;
+    });
+    setNotes(updatedNotes);
+  };
+
+  const handleEdit = (note: Note) => {
+    setNoteToEdit(note);
+  }
+
+  const handleDelete = (id: number) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  }
 
   return (
     <div className="layout-column align-items-center justify-content-start" data-testid="note-manager">
-      <NoteForm onSubmit={() => {}} noteToEdit={undefined} />
-      <NoteTable notes={notes} onDelete={() => {}} onEdit={() => {}} />
+      <NoteForm onSubmit={handleSubmit} noteToEdit={noteToEdit} />
+      <NoteTable notes={notes} onDelete={handleDelete} onEdit={handleEdit} />
     </div>
   );
 };
